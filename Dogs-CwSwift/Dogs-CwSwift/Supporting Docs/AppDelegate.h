005b9eb9bff81b948aa151f2b18472beeb85f93e
Michael Di Cesare <mvdicesare@gmail.com>
2019-10-09 14:07:18 -0600

    start

---
+++ Dogs-CwSwift/Dogs-CwSwift/Supporting Docs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dogs-CwSwift
+//
+//  Created by Michael Di Cesare on 10/9/19.
+//  Copyright © 2019 Michael Di Cesare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Dogs-CwSwift/Dogs-CwSwift/Supporting Docs/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Dogs-CwSwift
+//
+//  Created by Michael Di Cesare on 10/9/19.
+//  Copyright © 2019 Michael Di Cesare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
